2ce22c9854437b9d6df7418b3f60825474a3c8e1
Cristopher Bohol <cris.bacus6@gmail.com>
2022-05-14 10:35:53 +0800

    frontend_organize

--- car_rental_frontend/src/app/app-routing.module.ts
+++ car_rental_frontend/src/app/app-routing.module.ts
@@ -21,8 +21,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        canActivate:[AuthGuard],
-        component: HomeComponent,
+        component: DefaultLayoutComponent,
       },
       {
         path: 'home',
--- car_rental_frontend/src/environments/environment.ts
+++ car_rental_frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const DB_CREDENTIALS = require("../../my-key.json");
+const DB_CREDENTIALS = require("../../../my-key.json");
 export const environment = {
   production: false,
   firebase: {
